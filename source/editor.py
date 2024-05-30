f222a4ddbfd63978bb47adb0d51e0db62faa5ea2
Tucker Traywick <tucker.traywick@gmail.com>
2024-05-29 21:08:58 -0400

    Added basic cursor navigation.

--- source/editor.py
+++ source/editor.py
@@ -8,6 +8,13 @@ class Editor:
 		self.keepRunning = True
 		self.needsRedraw = True
 		self.document.buffer = Buffer()
+		self.keybindings = {
+			"q": self.quit,
+			"KEY_UP": self.cursorUpLine,
+			"KEY_DOWN": self.cursorDownLine,
+			"KEY_LEFT": self.cursorLeftCharacter,
+			"KEY_RIGHT": self.cursorRightCharacter,
+		}
 
 		self.open("source/example.txt")
 		self.terminal.pixel_width
@@ -27,12 +34,16 @@ class Editor:
 		changes = "[+] " if self.document.hasChanges else ""
 		status = f"{changes}{self.document.name} ({self.document.cursor.row}, {self.document.cursor.column}) | {self.document.fileType} | {self.document.lineEndingType}"
 		print(self.terminal.home + self.terminal.move_down(self.terminal.height), end="")
-		print(self.terminal.ljust(self.terminal.reverse + status), end="\r")
+		print(self.terminal.ljust(self.terminal.reverse + status), end="")
+		print(self.terminal.normal, end="")
 
 	# Draws the cursor.
 	def drawCursor(self):
+		y = self.document.cursor.row - self.document.scrollY
 		# TODO: Remove magic number.
-		print(self.terminal.home + self.terminal.move_yx(self.document.cursor.column, self.document.cursor.row + 4), end="", flush=True)
+		x = self.document.cursor.column - self.document.scrollX + 4
+		# TODO: Find a way to get the cursor to show up without flushing stdout.
+		print(self.terminal.home + self.terminal.move_yx(y, x), end="", flush=True)
 
 	# Draws the editor.
 	def draw(self):
@@ -41,13 +52,48 @@ class Editor:
 		self.drawStatusLine()
 		self.drawCursor()
 
+	# Processes key presses.
+	def processInput(self):
+		key = self.terminal.inkey().name
+		self.keybindings[key](self.terminal, key)
+
 	# The main loop for the editor. Keeps running until the user quits.
 	def run(self):
+		# TODO: Change `cbreak()` to `raw()` afer testing.
 		with self.terminal.fullscreen(), self.terminal.cbreak():
 			while self.keepRunning:
 				if self.needsRedraw:
 					self.needsRedraw = False
 					self.draw()
+				self.processInput()
+	
+	###############################
+	#### KEYBINDING FUNCTIONS #####
+	###############################
+	# Closes the file being edited and stops the editor.
+	def quit(self):
+		self.close()
+		self.keepRunning = False
+
+	# Moves the cursor up a line.
+	def cursorUpLine(self, terminal, key):
+		self.document.cursorUpLine(terminal, key)
+		self.needsRedraw = True
+
+	# Moves the cursor down a line.
+	def cursorDownLine(self, terminal, key):
+		self.document.cursorDownLine(terminal, key)
+		self.needsRedraw = True
+
+	# Moves the left a character.
+	def cursorLeftCharacter(self, terminal, key):
+		self.document.cursorLeftCharacter(terminal, key)
+		self.needsRedraw = True
+
+	# Moves the cursor up a line.
+	def cursorRightCharacter(self, terminal, key):
+		self.document.cursorRightCharacter(terminal, key)
+		self.needsRedraw = True
 
 # Holds a buffer and a cursor to operate on it.
 class Document:
@@ -61,6 +107,11 @@ class Document:
 		self.cursor = Cursor()
 		self.scrollY = 0
 		self.scrollX = 0
+
+	# Returns the line the cursor is on.
+	@property
+	def currentLine(self):
+		return self.buffer.lines[self.cursor.row]
 
 	# Opens a file and reads it into the buffer, and resets the cursor.
 	def open(self, path):
@@ -85,13 +136,60 @@ class Document:
 
 	# Draws the lines of the buffer to the terminal.
 	def draw(self, terminal):
-		self.buffer.draw(terminal)
+		self.buffer.draw(terminal, self.scrollY, self.scrollY + terminal.height - 1)
+
+	# Moves the cursor to the beginning of the line.
+	def cursorLineBegin(self, terminal, key):
+		self.cursor.column = 0
+		# TODO: Adjust horizontal scroll if needed.
+			
+	# Moves the cursor to the end of the line.
+	def cursorLineEnd(self, terminal, key):
+		self.cursor.column = len(self.currentLine)
+		# TODO: Adjust horizontal scroll if needed.
+
+	# Moves the cursor up a line and adjusts the scroll if needed.
+	def cursorUpLine(self, terminal, key):
+		if self.cursor.row > 0:
+			self.cursor.row -= 1
+			if self.cursor.row < self.scrollY:
+				self.scrollY -= 1
+
+	# Moves the cursor down a line and adjusts the scroll if needed.
+	def cursorDownLine(self, terminal, key):
+		if self.cursor.row < self.buffer.length - 1:
+			self.cursor.row += 1
+			if self.cursor.row > self.scrollY + terminal.height - 2:
+				self.scrollY += 1
+	
+	# Moves the cursor left a character.
+	def cursorLeftCharacter(self, terminal, key):
+		if self.cursor.column > 0:
+			self.cursor.column -= 1
+			# TODO: Decrement horizontal scroll if needed.
+		elif self.cursor.row > 0:
+			self.cursorUpLine(terminal, key)
+			self.cursorLineEnd(terminal, key)
+
+	# Moves the cursor right a character.
+	def cursorRightCharacter(self, terminal, key):
+		if self.cursor.column < len(self.currentLine):
+			self.cursor.column += 1
+			# TODO: Increment horizonal scroll if needed.
+		elif self.cursor.row < self.buffer.length - 1:
+			self.cursorDownLine(terminal, key)
+			self.cursorLineBegin(terminal, key)
 
 # A list of lines that represents a piece of text.
 class Buffer:
 	def __init__(self):
 		self.lines = []
 
+	# Returns how many lines are in the buffer.
+	@property
+	def length(self):
+		return len(self.lines)
+	
 	# Resets the state of the buffer.
 	def reset(self):
 		self.lines = []
@@ -102,9 +200,9 @@ class Buffer:
 		# TODO: Handle failed `read()`.
 
 	# Draws the lines of the buffer to the terminal.
-	def draw(self, terminal):
-		for i, line in enumerate(self.lines):
-			print(f"{i + 1:>3} {line}")
+	def draw(self, terminal, start, end):
+		for i, line in enumerate(self.lines[start:end]):
+			print(f"{start + i + 1:>3} {line}")
 
 # A location in a buffer.
 class Cursor:
